9084d3acd730510e54319c700a78ca4a6742c767
boonyong <boonyong.kang93@gmail.com>
2024-11-21 21:17:09 +0800

    Add `summary` field to `Post` model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -33,6 +33,7 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
+    summary = models.TextField(default="")
 
     # * Model managers
     # First manager defined becomes default manager.
